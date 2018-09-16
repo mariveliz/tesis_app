06022d5327d00f909a5884f2a517a79fa2ba41e5
mariveliz <cristin_1987_02@hotmail.com>
2018-09-16 15:44:25 -0300

    [firebase] + firestore

--- android/app/src/main/java/com/mdd/MainApplication.java
+++ android/app/src/main/java/com/mdd/MainApplication.java
@@ -5,16 +5,19 @@ import android.app.Application;
 import com.facebook.react.ReactApplication;
 import io.invertase.firebase.RNFirebasePackage;
 import io.invertase.firebase.auth.RNFirebaseAuthPackage;
+import io.invertase.firebase.firestore.RNFirebaseFirestorePackage;
 import com.airbnb.android.react.maps.MapsPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
+import android.support.multidex.MultiDexApplication;
 
 import java.util.Arrays;
 import java.util.List;
 
-public class MainApplication extends Application implements ReactApplication {
+public class MainApplication extends MultiDexApplication implements ReactApplication {
+
 
   private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
     @Override
@@ -28,7 +31,7 @@ public class MainApplication extends Application implements ReactApplication {
           new MapsPackage(),
           new MainReactPackage(),
           new RNFirebasePackage(),
-          new RNFirebaseAuthPackage() 
+          new RNFirebaseAuthPackage()
       );
     }
 
